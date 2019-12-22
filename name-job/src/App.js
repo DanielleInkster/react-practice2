312e57bc458cf2263a4347a753f1e36db4dd02a1
DanielleInkster <danielle.inkster@gmail.com>
2019-12-22 23:57:59 +0000

    Basic forms - no submit

--- name-job/src/App.js
+++ name-job/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
     state = {
@@ -23,6 +24,7 @@ class App extends Component {
         return (
             <div className="container">
                 <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form />
             </div>
         )
     }
